9ddc20c0a699eace0904542c29ec6f2362d80946
arnabbhat82 <arnabbhat@gmail.com>
2020-09-04 08:44:30 +0530

    var let example

---
+++ src/app/let_const.js
@@ -0,0 +1,13 @@
+// exampler for var starts
+// var myName = "manu";
+// console.log(myName);
+
+// myName = "Max";
+// console.log(myName);
+// exampler for var ends
+
+const myName = "manu";
+console.log(myName);
+
+myName = "Max";
+console.log(myName);
